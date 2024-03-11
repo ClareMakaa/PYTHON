97bab45199e732dd090fda34ccdb7bbb7733de62
Clare Makaa <claremakaa1@gmail.com>
2024-03-11 22:31:09 +0300

    User input in Python User input in Python

--- Python input.py
+++ Python input.py
@@ -1,3 +1,7 @@
 num = input ("Enter a number: ")
 print("You entered:", num)
 print("Data type of num:", type(num))
+
+name = input("Enter your name: ")
+print("Your name is " + name)
+print(type(name))
